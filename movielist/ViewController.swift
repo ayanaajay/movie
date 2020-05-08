a8203f9eba0bd7c45d056a9d42279e871aa5469c
Ayana <ayana@tests-MacBook-Pro.local>
2020-05-08 16:24:56 +0530

    Initial Commit

---
+++ movielist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movielist
+//
+//  Created by Ayana on 08/05/20.
+//  Copyright © 2020 Ayana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
